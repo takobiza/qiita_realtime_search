6b7ba3c84a74a21634cc96b82f63af7aef35fa8f
takobiza <hantoshi@jiboujikinoMacBook-ea.local>
2019-04-28 22:15:37 +0900

    First commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      items: null,
+      keyword: '',
+      message: ""
+    },
+    watch: {
+      keyword: function(newKeyword, oldKeyword) {
+        this.message = 'Waiting...'
+        this.debouncedGetAnswer()
+      }
+    },
+    created: function() {
+      this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+    },
+    methods: {
+      getAnswer: function() {
+        if(this.keyword === '') {
+          this.items = null
+          this.message = ''
+          return
+        }
+
+        this.message = 'Loading...'
+        var vm = this
+        var params = { page: 1 , per_page: 20, query: this.keyword}
+        axios.get('https://qiita.com/api/v2/items',{ params})
+          .then(function(response){
+            vm.items = response.data
+          })
+          .catch(function(error){
+            vm.message = 'Error!' + error
+          })
+          .finally(function(){
+            vm.message = ''
+          })
+      }
+    }
+})
